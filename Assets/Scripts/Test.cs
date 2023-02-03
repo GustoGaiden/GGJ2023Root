7fa342cfa8ea59cbd99a5aab3e15ef518818478d
Andy Michael <andy.michael@freiheit.com>
2023-02-03 22:52:36 +0100

    Hello WOrld

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -7,12 +7,12 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("HELLO WORLD");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("DIG!");
     }
 }
