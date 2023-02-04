64642c84fc7e6458a2ef7683974645dcc885a26e
Philipp-Rueter <philippr90@web.de>
2023-02-04 14:51:55 +0100

    add collision logic, but it isn't working

--- Assets/Scripts/CollisionLogic.cs
+++ Assets/Scripts/CollisionLogic.cs
@@ -4,26 +4,26 @@ using UnityEngine;
 
 public class CollisionLogic : MonoBehaviour
 {
-    void OnTriggerEnter2D(Collider2D collision) => HandleCollision(collision);
+    void OnTriggerEnter2D(Collider2D collision) => LogCollision(collision);
+    void OnTriggerStay2D(Collider2D collision) => LogCollision(collision);
+    void OnTriggerExit2D(Collider2D collision) => LogCollision(collision);
 
-    public PlayerController? PlayerController;
-
-    private void HandleCollision(Collider2D collision)
+    void LogCollision(Collider2D collision)
     {
-        if(PlayerController != null)
-        {
-            // Debug.Log($"Collision handling for player object {collision.name}");
-            PlayerController.ResetForNewRun();
-        }
+        Debug.Log(
+            $"{collision.gameObject.name} detected a collision"
+            );
     }
 
-    // Start is called before the first frame update
+        // Start is called before the first frame update
     void Start()
     {
+        Debug.Log($"collision logic started for {name}");
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log("update happened");
     }
 }
