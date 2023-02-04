c2d3cd1ed55c9e6637b38e02cf57b00e75daa206
Philipp-Rueter <philippr90@web.de>
2023-02-04 16:35:23 +0100

    make collision logic work

--- Assets/Scripts/CollisionLogic.cs
+++ Assets/Scripts/CollisionLogic.cs
@@ -4,26 +4,15 @@ using UnityEngine;
 
 public class CollisionLogic : MonoBehaviour
 {
-    void OnTriggerEnter2D(Collider2D collision) => LogCollision(collision);
-    void OnTriggerStay2D(Collider2D collision) => LogCollision(collision);
-    void OnTriggerExit2D(Collider2D collision) => LogCollision(collision);
+    void OnTriggerEnter2D(Collider2D collision) => Debug.Log($"Collision occured for {collision.name}");
 
-    void LogCollision(Collider2D collision)
-    {
-        Debug.Log(
-            $"{collision.gameObject.name} detected a collision"
-            );
-    }
-
-        // Start is called before the first frame update
+    // Start is called before the first frame update
     void Start()
     {
-        Debug.Log($"collision logic started for {name}");
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("update happened");
     }
 }
