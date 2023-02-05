8e5961862c947340acc3fb7433f093350c4aa217
Andy Michael <andy.michael@freiheit.com>
2023-02-05 14:24:33 +0100

    add tutorial to main scene

--- Assets/Scripts/TutorialText.cs
+++ Assets/Scripts/TutorialText.cs
@@ -15,11 +15,11 @@ public class TutorialText : MonoBehaviour
     private List<string> TutorialTexts = new List<string>()
     {
         "Press RIGHT ARROW for TUTORIAL",
-        "(1/5) Starting Out\npress SPACEBAR to Start\n press A and D keys to Steer your Tendril",
-        "(2/5) Caverns & Juice\nExplore the earth to find Caverns.\nCaverns increase your maximum Juice.\nExploring Deeper costs more Juice.",
-        "(3/5) Traversing\nYou can Traverse your old roots!\n Press A or D to continue down a Branch.\n Traversing costs less Juice.",
-        "(4/5) Branching Out\n While Traversing: \nPress SHIFT with the A or D keys to Branch off\nand and Explore new areas.",
-        "(5/5) Debug Commands\n Press BACKSPACE to end your run early."
+         "Starting Out (1/5)\npress SPACEBAR to Start\n press A and D keys to Steer your Tendril",
+        "Caverns & Juice (2/5)\nExplore the earth to find Caverns.\nCaverns increase your maximum Juice.\nExploring Deeper costs more Juice.",
+        "Traversing (3/5)\nYou can Traverse your old roots!\n Press A or D to continue down a Branch.\n Traversing costs less Juice.",
+        "Branching Out (4/5)\n While Traversing: \nPress SHIFT with the A or D keys to Branch off\nand and Explore new areas.",
+        "Debug Commands (5/5)\n Press BACKSPACE to end your run early."
     };
 
     private void Start()
